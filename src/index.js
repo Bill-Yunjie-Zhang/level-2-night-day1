389e7a0b759667c3685d47320d511e4fa07adea9
Bill <207559339@qq.com>
2019-09-21 20:47:09 +0800

    if

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ var addNumbers = function(x, y){
 
 var testAddNumbers = addNumbers(5, 6)
 console.log(testAddNumbers)
+
+if(true){
+    console.log("is true")
+}else{
+    console.log("if false")
+}
+
+if (testAddNumbers > 20){
+    console.log("is true")
+} else {
+    console.log("is false")
+}
